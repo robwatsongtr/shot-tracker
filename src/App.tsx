d8a9f313beb4523ef3b4dc9a7916176dd88a770b
robwatsongtr <rwatso@gmail.com>
2023-06-01 12:03:19 -0700

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="grid-area item-1">
+        <span>Text 1</span>
+        <span>Text 2</span>
+      </div>
+      <div className="grid-area item-2">
+        <span>Text 1</span>
+        <span>Text 2</span>
+      </div>
+      <div className="grid-area item-3">
+        <span>Text 1</span>
+        <span>Text 2</span>
+      </div>
+      <div className="grid-area item-4">
+        <span>Text 1</span>
+        <span>Text 2</span>
+      </div>
     </div>
   );
 }

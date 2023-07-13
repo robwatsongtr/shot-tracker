f59c683f9a988c3c536cd43a92f204c3f1648b98
robwatsongtr <rwatso@gmail.com>
2023-07-13 14:42:25 -0700

    adding history page and react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import MainPage from "./components/MainPage"
-
+import { Routes, Route } from 'react-router-dom';
+import History from "./components/History"
 
 const App = () => {
   return (
-   <MainPage />
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/history" element={<History />} />
+    </Routes>
   );
 }
 
---
+++ src/components/History.tsx
@@ -0,0 +1,6 @@
+
+const History = () => {
+  return <h2>History</h2>
+}
+
+export default History

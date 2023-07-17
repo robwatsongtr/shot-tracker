57c39fe4a67933297d381aacc0f99335a4b87145
robwatsongtr <rwatso@gmail.com>
2023-07-17 09:10:17 -0700

    cleaned up code

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import MainPage from "./components/MainPage"
 import { Routes, Route } from 'react-router-dom';
-import HistoryText from "./components/HistoryText"
 import HistoryTable from "./components/HistoryTable"
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
-      <Route path="/historyText" element={<HistoryText />} />
       <Route path="/historyTable" element={<HistoryTable />} />
     </Routes>
   );

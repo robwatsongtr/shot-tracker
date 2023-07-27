628e765d1dde8da6be8ab0cb3ef2a053a2c11502
robwatsongtr <rwatso@gmail.com>
2023-07-27 12:53:11 -0700

    renamed Login to LoginPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import MainPage from "./components/MainPage"
 import { Routes, Route } from 'react-router-dom';
 import HistoryTable from "./components/HistoryTable"
-import Login from "./components/Login"
+import LoginPage from "./components/LoginPage"
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<LoginPage />} />
       <Route path="/mainPage" element={<MainPage />} />
       <Route path="/historyTable" element={<HistoryTable />} />
     </Routes>
--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 const auth = getAuth()
 
-const Login = () => {
+const LoginPage = () => {
   const navigate = useNavigate()
 
   useEffect(() => {
@@ -35,4 +35,4 @@ const Login = () => {
   )
 }
 
-export default Login 
+export default LoginPage 
